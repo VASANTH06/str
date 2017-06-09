0f815ac7bbdcc1bf8193ca29ffcb797c0b5a3fe0
VASANTH06 <vasanthrgm@gmail.com>
2017-06-09 15:10:37 +0530

    Create Str.java

---
+++ Str.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.lang.*;
+class Str
+{
+    public static void main(String[] args) {
+        StringBuffer a=new StringBuffer("hello");  
+        StringBuffer b=a.reverse();
+        System.out.println(b);
+    }
+    
+}
